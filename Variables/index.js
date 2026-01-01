3b34c7197f4cd93e0f0f35ff995e83832ff409ff
Kunal Rijhonia <krijhonia@gmail.com>
2026-01-01 13:46:52 +0530

    feat: add Variables exercises

---
+++ Variables/index.js
@@ -0,0 +1,35 @@
+//Variable = A container that stores a value 
+//           Behaves as if it were value it contains
+
+// 1. declaration let x;
+// 2. assingment  x = 100;
+
+
+//* Number data type
+let x; 
+x = 100;
+console.log(x);
+
+let age = 25;
+console.log(age);
+
+let price = 10.99;
+console.log(price);
+
+let gpa = 2.1
+console.log(gpa);
+
+
+//For printing the string along with variables
+console.log(`You are ${age} years old`);
+console.log(`The price is ${price}`);
+console.log(`Your gpa is : ${gpa}`);
+
+//For printing the data types of the variables
+console.log(typeof age)
+
+//* String data type
+
+
+
+
