2fe1b82a47a7ee0e67ed129abf8a6f36bed57e22
Kunal Rijhonia <krijhonia@gmail.com>
2026-01-04 09:01:04 +0530

    Added constants and counter program

--- Constants/index.js
+++ Constants/index.js
@@ -1,10 +1,14 @@
 //const = a variable that can't be changed
 
-let pi = 3.14159;
+const PI = 3.14159;
 let radius;
 let circumference;
 
-radius = window.prompt("What's the radius of the circle");
-radius = Number(radius);
-circumference = 2 * pi * radius;
-console.log(circumference)
+
+document.getElementById('mySubmit').onclick = function(){
+    radius = document.getElementById('myText').value;
+    radius = Number(radius);
+    circumference = 2 * PI * radius;
+    document.getElementById('myH3').textContent = circumference + "cm";
+
+}
